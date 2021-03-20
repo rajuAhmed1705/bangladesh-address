82a390879ee3158b2c22912fc78d8c8e13dd9665
raju1705 <rajucse1705@gmail.com>
2021-03-20 11:55:29 +0600

    refactored params

--- src/division/division.ts
+++ src/division/division.ts
@@ -2,8 +2,8 @@ import data from "../json/bd-upazila.json";
 import _ from "lodash";
 import { DivisonName } from "./types/division-name";
 
-export const getDivision = (name: DivisonName) => {
-    return _.filter(data, {
-        division: _.upperFirst(name),
-    });
+export const getDivision = (name: DivisonName | string) => {
+  return _.filter(data, {
+    division: _.upperFirst(name),
+  });
 };
